60f1cdf246a30394fba79a72e49a6e85bc39398a
Treyser044 <treyser044@gmail.com>
2025-06-26 11:15:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let string = prompt('string typidagi malumot kiriting')
+let number = prompt('Number typedagi malumot kiriting')
+let boolean = prompt('boolean typedagi malumot kiriting')
+alert('malumotlarni togri kiritkaningizga ishonchiz komil mi?')
+
++alert('siz kiritgan ' + string + ' sozi string typega tegishli' + '\n' + 'Siz kiritgan ' + number + ' soni number typega tegishli' + '\n' + 'siz kiritgan ' + boolean + ' sozi boolean typega tegishli')
